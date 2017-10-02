a656efa43eadc800783fe6b35f9ff42747fe23ea
stephane joel <stephanejoel1986@yahoo.fr>
2017-10-02 14:17:28 +0200

    hello Leute

---
+++ HelloWorld/src/de/firma/HelloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+package de.firma.HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
